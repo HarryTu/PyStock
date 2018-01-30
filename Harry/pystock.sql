418e13c104345f58a026de72d68f9af632ef0a04
HarryTu <49756308@qq.com>
2018-01-30 17:25:10 +0800

    upload mystock sql file

---
+++ Harry/pystock.sql
@@ -0,0 +1,52 @@
+create table stocks(
+code varchar(7),
+codealias varchar(10),
+name varchar(30),
+scope varchar(50),
+circulated float,
+totalstock float,
+status int,
+peg float,
+lyr float,
+mtime datetime
+);
+
+alter table stocks add primary key(code);
+
+
+create table mystocks(
+code varchar(7),
+codealias varchar(10),
+name varchar(10),
+cashin float,
+cashout float,
+initnetvalue float,
+netvalue float,
+iorate float,
+turnover float,
+price float,
+initchangeratio float,
+changeratio float,
+amountp float,
+amountn float,
+mtime datetime,
+inittime datetime		
+);
+
+alter table mystocks add primary key(code)
+
+create table rtstocks(
+code varchar(7),
+cashin float,
+cashout float,
+netvalue float,
+iorate float,
+turnover float,
+price float,
+changeratio float,
+amountp float,
+amountn float,
+mtime datetime		
+);
+
+alter table rtstocks add primary key(code,time);
